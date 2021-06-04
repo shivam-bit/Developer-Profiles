3be3f37134f178bc21994d64705a8b3fb414be3e
shivam singh <shivama5tronot@gmail.com>
2021-06-04 17:52:30 +0530

    Added modal box

---
+++ public/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+window.onload = () => {
+    const triggerBtn = document.querySelector('.addDevSection__btn');
+    const modalBox = document.querySelector('.modal');
+    const closeBtn = document.querySelector('.close-button');
+    triggerBtn.addEventListener('click', () => {
+        console.log('event attached');
+        modalBox.classList.add('show-modal');
+    });
+    closeBtn.addEventListener('click', () => {
+        console.log('event attached');
+        modalBox.classList.remove('show-modal');
+    });
+};
